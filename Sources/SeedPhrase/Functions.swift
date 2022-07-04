8d80aeba7b2cb8beb3632faaf62323dd8b7fdac6
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-04 13:31:46 +0300

    Created bin2dec function

--- Sources/SeedPhrase/Functions.swift
+++ Sources/SeedPhrase/Functions.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  Functions.swift
 //  
 //
 //  Created by Олег Рыбалко on 04.07.2022.
 //
 
 import Foundation
+
+
+func bin2dec(_ bits: [Bit]) -> Int {
+    var res = 0
+    let N = bits.count - 1
+    for i in stride(from: N, to: -1, by: -1) {
+        switch bits[i] {
+        case .zero:
+            break
+        case .one:
+            res += Int(pow(2.0, Double(N - i)))
+        }
+    }
+    return res
+}
