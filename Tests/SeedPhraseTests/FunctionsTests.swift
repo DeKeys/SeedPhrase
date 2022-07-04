fa684c7cb4a5ec08f64b269574bf5baef09672f7
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-04 13:31:31 +0300

    Created tests for bin2dec function

--- Tests/SeedPhraseTests/FunctionsTests.swift
+++ Tests/SeedPhraseTests/FunctionsTests.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  FunctionsTests.swift
 //  
 //
 //  Created by Олег Рыбалко on 04.07.2022.
 //
 
 import Foundation
+import XCTest
+@testable import SeedPhrase
+
+final class FunctionsTests: XCTestCase {
+    func testConvertbin2dec() {
+        let oneBits: [Bit] = [.zero, .zero, .one]
+        let twoBits: [Bit] = [.one, .zero]
+        let nullBits: [Bit] = []
+        let maxBits = [Bit](repeating: .one, count: 11)
+        XCTAssertEqual(bin2dec(oneBits), 1)
+        XCTAssertEqual(bin2dec(twoBits), 2)
+        XCTAssertEqual(bin2dec(nullBits), 0)
+        XCTAssertEqual(bin2dec(maxBits), 2047)
+    }
+}
