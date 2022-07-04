beb359e941a8846b1476a8b84ad5ecc09817ed5a
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-04 12:22:47 +0300

    Cleared tests

--- Tests/SeedPhraseTests/SeedPhraseTests.swift
+++ Tests/SeedPhraseTests/SeedPhraseTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import SeedPhrase
 
 final class SeedPhraseTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SeedPhrase().text, "Hello, World!")
-    }
+    
 }
