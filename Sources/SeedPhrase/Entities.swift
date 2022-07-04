488f829bfb4fcdf817efcd61ce57148822264125
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-04 12:20:34 +0300

    Added extensions for getting bits from data

---
+++ Sources/SeedPhrase/Entities.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Олег Рыбалко on 04.07.2022.
+//
+
+import Foundation
---
+++ Sources/SeedPhrase/Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  Extensions.swift
+//  
+//
+//  Created by Олег Рыбалко on 04.07.2022.
+//
+
+import Foundation
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : PowerPrecedence
+func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
+}
+
+// https://gist.github.com/pofat/6ae0c2626660741234f159c60f51af91
+extension Data {
+    var bytes: [Byte] {
+        var byteArray = [UInt8](repeating: 0, count: self.count)
+        self.copyBytes(to: &byteArray, count: self.count)
+        return byteArray
+    }
+}
+
+extension Byte {
+    var bits: [Bit] {
+        let bitsOfAbyte = 8
+        var bitsArray = [Bit](repeating: Bit.zero, count: bitsOfAbyte)
+        for (index, _) in bitsArray.enumerated() {
+            // Bitwise shift to clear unrelevant bits
+            let bitVal: UInt8 = 1 << UInt8(bitsOfAbyte - 1 - index)
+            let check = self & bitVal
+            
+            if check != 0 {
+                bitsArray[index] = Bit.one
+            }
+        }
+        return bitsArray
+    }
+}
